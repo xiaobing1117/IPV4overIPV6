d39540669633955270b258b544dee28831ff4d9f
xiaobing1117 <lianwh14@mails.tsinghua.edu.cn>
2017-05-02 14:21:17 +0800

    initialized

---
+++ app/src/main/java/experiment/network/ipv4overipv6/MainActivity.java
@@ -0,0 +1,13 @@
+package experiment.network.ipv4overipv6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
